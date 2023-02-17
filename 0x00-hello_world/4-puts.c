6c459288e3b9bc58aa3110774e1bc62ca7ef6738
Talent12-qfk <yobogift9@gmail.com>
2023-02-16 18:42:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *  * main - entry point
  *   *prints "Programming is like building a multilingual puzzle
  *    * Return: 0 if exited properly, non-zero otherwise
- *     */
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
