1b341bc0df22736d4f3a20891f994a8f98a1ee3b
Talent12-qfk <yobogift9@gmail.com>
2023-02-18 13:33:15 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  * main - Entry Point
+ *   * Description: Print all numbers of base 10, starting from 0.
+ *    * Return: 0 (Always) Successful
+ *     */
+int main(void)
+{
+		int i;
+
+for (i = 0; i < 10; i++)
+					{
+								printf("%i", i);
+
+}
+putchar('\n');
+                              
+return (0);
+} 
