ddf7142815b1e00d3a5643438b1e1fcf8de4ee5b
Talent12-qfk <yobogift9@gmail.com>
2023-02-20 07:45:56 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,17 @@
- #include <stdio.h>
+#include <stdio.h>
 /**
- *  * main - Entry point
- *   * Description: Print the alphabet in reverse order in lowercase
- *    * Only putchar functions allowed, and putchar only twice
- *     * Return: 0 if successful (Always), nonzero otherwise
- *      */
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		char c;
+	char ch;
 
-			for (c = 'z'; c >= 'a'; c--)
-					{
-								putchar(c);
-									}
-				putchar('\n');
-
-					return (0);
-} 
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
