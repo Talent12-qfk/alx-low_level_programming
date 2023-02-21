e376a1119751b0e8c2a527cc8cf1084c2cadbe54
Talent12-qfk <yobogift9@gmail.com>
2023-02-21 09:35:31 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+ #include "main.h"
+/**
+ *  * Main: Entry point
+ *   * Description:Write a program that prints '_putchar' followed by a newline
+ *    * Return 0: Successful (Always), non-zero otherwise
+ *     */
+int main(void)
+{
+		char SWMuriu[] = "_putchar";
+			int j = 0;
+
+				while (SWMuriu[j] != '\0')
+						{
+									_putchar(SWMuriu[j]);
+											j++;
+
+						}
+_putchar('\n');
+
+						return (0);
+}
