f05f280a40171c235e7daaaae34e65a9e6e316e1
Talent12-qfk <yobogift9@gmail.com>
2023-02-21 11:07:20 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
- #include "main.h"
+#include "main.h"
 /**
- *  * Main: Entry point
- *   * Description:Write a program that prints '_putchar' followed by a newline
- *    * Return 0: Successful (Always), non-zero otherwise
- *     */
+* Main: Entry point
+* Description:Write a program that prints '_putchar' followed by a newline
+* Return 0: Successful (Always), non-zero otherwise
+*/
 int main(void)
 {
-		char SWMuriu[] = "_putchar";
-		int j = 0;
+	char SWMuriu[] = "_putchar";
+	int j = 0;
 
-while (SWMuriu[j] != '\0')
-						{
+while(SWMuriu[j] != '\0')
+{
 _putchar(SWMuriu[j]);
-j++;						}
-
+j++;
+}
 _putchar('\n');
-
 return (0);
 } 
