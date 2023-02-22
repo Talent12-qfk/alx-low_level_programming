22be9f3746d912bb51ed0eab7b2a3e87b0ea5c57
Talent12-qfk <yobogift9@gmail.com>
2023-02-22 01:34:46 -0800

    5-sign.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,11 +9,11 @@ int main(void)
 	char SWMuriu[] = "_putchar";
 	int j = 0;
 
-while(SWMuriu[j] != '\0')
-{
+while(SWMuriu[j] != '\0') 
+{ 
 _putchar(SWMuriu[j]);
 j++;
-}
+} 
 _putchar('\n');
 return (0);
 } 
