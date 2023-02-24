17e207234cca5789231bf825cdd6368d3fbee681
Talent12-qfk <yobogift9@gmail.com>
2023-02-23 23:58:54 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - prints largest prime factor.
+* Return: Always 0.
+*/
+int main(void)
+{
+long int n, f;
+n = 612852475143;
+for (f = 2; f <= n; f++)
+{
+if (n % f == 0)
+{
+n /= f;
+f--;
+}
+}
+printf("%ld\n", f);
+return (0);
+}
