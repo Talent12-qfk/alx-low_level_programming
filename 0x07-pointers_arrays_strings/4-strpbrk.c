e32579393ae7f2450cccf465bf64dfa07afc6947
Talent12-qfk <yobogift9@gmail.com>
2023-03-06 13:17:23 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - funtion searches str for any of a set of bytes
+ * @s: string in question
+ * @accept: set containing the bytes in question
+ *
+ * Return: pointer to bytes found, NULL if no bytes found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int k;
+while (*s)
+{
+for (k = 0; accept[k]; k++)
+{
+if (*s == accept[k])
+return (s);
+}
+s++;
+}
+eturn ('\0');
+}
